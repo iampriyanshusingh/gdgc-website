6ac2cfa824b70c3289778287d84048a2699089a0
RelaxItsAryan <aryanarya5507@gmail.com>
2026-02-15 19:55:02 +0530

    Fixed Bg of FAQ section

--- src/Advitya26Components/AdvityaFAQs.jsx
+++ src/Advitya26Components/AdvityaFAQs.jsx
@@ -15,11 +15,20 @@ function AdvityaFAQs() {
     }, []);
 
     return (
-        <div className="min-h-screen bg-black">
+        <div
+            className="min-h-screen w-screen relative"
+            style={{
+                minHeight: '100vh',
+                width: '100vw',
+                backgroundImage: "url('/FAQ_BG.png')",
+                backgroundSize: '100% 100%',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+            }}
+        >
             <div className="absolute top-0 left-0 w-full h-[10vh] z-50">
                 <GlobalNavbar />
             </div>
-            
             {/* FAQ Content */}
             <div className="pt-20">
                 <FAQ />
